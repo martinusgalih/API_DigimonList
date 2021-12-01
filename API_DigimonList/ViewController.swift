bbe9c5d0a4697a15ce895082b0fe9469fa052384
martinusgalih <martinusgalih98@gmail.com>
2021-12-02 00:54:43 +0700

    Initial Commit

---
+++ API_DigimonList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API_DigimonList
+//
+//  Created by Martinus Galih Widananto on 02/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
